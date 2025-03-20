54cf789d90daed672ada1b2eac55500d9dc4b8e0
wiidede <wiixdede@gmail.com>
2025-03-20 20:51:36 +0800

    chore: release prepare

--- rollup.config.js
+++ rollup.config.js
@@ -1,53 +1,32 @@
-import process from 'node:process'
 import json from '@rollup/plugin-json'
-import resolve from '@rollup/plugin-node-resolve'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 import terser from '@rollup/plugin-terser'
 import { defineConfig } from 'rollup'
-import builtins from 'rollup-plugin-node-builtins'
-import globals from 'rollup-plugin-node-globals'
 
-const plugins = [
-  resolve({
-    browser: true,
-    jsnext: true,
-    main: true,
-    module: true,
-    preferBuiltins: true,
-  }),
-  builtins(),
-  globals(),
-  replace({
-    vars: {
-      ENV: process.env.NODE_ENV || 'development',
+export default defineConfig({
+  input: 'src/ebml/index.js',
+  output: [
+    {
+      file: 'dist/ebml.js',
+      format: 'es',
+      sourcemap: true,
     },
-    preventAssignment: true,
-  }),
-  json(),
-]
-
-const sourcemap = process.env.SOURCE_MAPS || true
-
-export default defineConfig([
-  {
-    input: './src/ebml/index.js',
-    output: [
-      {
-        file: 'lib/ebml.esm.js',
-        format: 'esm',
-        sourcemap,
-      },
-    ],
-    plugins,
-  },
-  {
-    input: './src/ebml/index.js',
-    output: [
-      {
-        file: 'lib/ebml.esm.min.js',
-        format: 'esm',
-      },
-    ],
-    plugins: [...plugins, terser()],
-  },
-])
+    {
+      file: 'dist/ebml.min.js',
+      format: 'es',
+      sourcemap: true,
+      plugins: [terser()],
+    },
+  ],
+  plugins: [
+    nodeResolve({
+      browser: true,
+      preferBuiltins: false,
+    }),
+    replace({
+      preventAssignment: true,
+    }),
+    json(),
+  ],
+})
